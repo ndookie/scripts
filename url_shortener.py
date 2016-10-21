f92745c5bc0fcda3397d639bd0baef9695208c4a
Nigel Dookie <ndookie@gmail.com>
2016-10-20 21:08:34 -0400

    Fully working hash generator and lookup functions, ready to incorporated into flask.

--- url_shortener.py
+++ url_shortener.py
@@ -2,19 +2,23 @@
 
 # Importing integer base converter.
 from base_converter import base_conversion
+from base_converter import hash_to_number
 
 import sqlite3 as lite
+import sys
 
-# ASCII to INT Map
-
+base = 62
 
 # Database set up
-con = None
+try:
+	con = None
+	con = lite.connect('shortener.db');
+	cur = con.cursor()
+except:
+	print "Connection to database has failed."
+	sys.exit()
 
-con = lite.connect('shortener.db');
-
-cur = con.cursor()
-
+# Function for inserting new URL into database.
 def insert_url(url):
 	cur.execute("INSERT INTO url_table VALUES(NULL,'%s',NULL)" % (url))
 
@@ -26,31 +30,46 @@ def insert_url(url):
 
 	return url_id[0]
 
+# Generates hash based off id from table.
 def generate_hash(id):
-	new_hash = base_conversion(id,62)
+	new_hash = base_conversion(id,base)
 
 	return new_hash
 
+# Updates the entry for a specific ID with it's hash.
 def update_shortened_url(id, hash):
 	cur.execute("UPDATE url_table SET shortened_url='" + hash + "' WHERE id=%i" % (id));
 
 	con.commit()
+	return
 
-def url_fetcher(hash) {
-	# need to write this, include changing to base 10
-}
+# Fetches corresponding URL for hash from database.
+def url_fetcher(hash):
+	fetching_id = hash_to_number(hash, base)
 
+	cur.execute("SELECT url FROM url_table WHERE id=" + str(fetching_id))
+	url = cur.fetchall()
+	url = url[-1]
+
+	con.close()
+
+	return url[0]
+
+
+# Takes a url, inserts it into the database and returns the hash.
 def url_generator(url):
 	new_id = insert_url(url)
-
 	shortened_url = generate_hash(new_id)
 	update_shortened_url(new_id, shortened_url)
 
 	con.close()
 
-	return
+	return shortened_url
 
+#hash_to_number("Fgn", 62)
+#print base_conversion(123456, 62)
 
+print url_fetcher("L")
 
-url_generator('friendssdfdsfdhip.com')
+#url_generator('nigelriazdookie.com')
 
